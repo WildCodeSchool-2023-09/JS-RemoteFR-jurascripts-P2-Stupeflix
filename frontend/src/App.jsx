3a60b0d1687b3c714081967ddd9b51ebdfb02ee9
FredP21 <f.pelletier21@gmail.com>
2023-10-25 14:42:42 +0200

    Create gameUI components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,10 @@
-
-
+import Header from "./components/Header";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-     
-      </header>
+     <Game />
     </div>
   );
 }
---
+++ frontend/src/components/Game.jsx
@@ -0,0 +1,56 @@
+import { AiOutlineInfo } from "react-icons/ai";
+import { BsFillGearFill } from "react-icons/bs"; 
+import { useState } from "react";
+import InfoCharacter from "./InfoCharacter";
+import InfoDay from "./InfoDay";
+
+function Game() {
+  const [info, setInfo] = useState(true);
+  const toggleInfo = () => {
+    setInfo(!info);
+  };
+
+  const [params, setParams] = useState(true);
+  const toggleParams = () => {
+    setParams(!params);
+  };
+
+  return (
+    <div className="container-game-parent">
+      <div className="container-game">
+        <div className="container-icons">
+          <AiOutlineInfo className="container-icon-i" onClick={toggleInfo} />
+          <BsFillGearFill
+            className="container-icon-gear"
+            onClick={toggleParams}
+          />
+        </div>
+        {params && (
+          <div className="container-params-info">
+            <h3>Paramètres</h3>
+            <p>Volume de la music: 10/10</p>
+            <p>Volume des voix des personnages: 10/10</p>
+            <p>lorem</p>
+            <p>lorem</p>
+          </div>
+        )}
+        <h1 className="container-game-h1" onClick={toggleInfo}>
+          HOGWARTS DAY
+        </h1>
+        <a href="">
+          <button className="container-game-button">Lancer la partie</button>
+        </a>
+        {info && (
+          <div>
+            <div className="container-info">
+              <InfoDay />
+              <InfoCharacter />
+            </div>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default Game;
