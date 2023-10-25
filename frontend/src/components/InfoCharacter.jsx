6ce87392b16afc76ee9773e672d35da7d876fbc1
FredP21 <f.pelletier21@gmail.com>
2023-10-25 14:30:06 +0200

    Create Infos components

---
+++ frontend/src/components/InfoCharacter.jsx
@@ -0,0 +1,12 @@
+function InfoCharacter() {
+    return (
+      <div className="container-info-character">
+        <h3 className="container-info-character-h3">Info de mon personnage</h3>
+        <p className="container-info-character-p">Vie: 80%</p>
+        <p className="container-info-character-p">Attaque: 110%</p>
+        <p className="container-info-character-p">Défense 93%</p>
+      </div>
+    );
+  }
+  
+  export default InfoCharacter;
