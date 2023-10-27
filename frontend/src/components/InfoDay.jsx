5265482d0f3fb5547264c81027a4c74ef0902a55
Tetardtek <kvnn64@gmail.com>
2023-10-27 11:25:57 +0200

    Base project Step1

---
+++ frontend/src/components/InfoDay.jsx
@@ -0,0 +1,12 @@
+function InfoDay() {
+  return (
+    <div className="container-info-day">
+      <h3 className="container-info-day-h3">Info de la journée</h3>
+      <p className="container-info-day-p">Point cumulé:</p>
+      <p className="container-info-day-p">Lieu: </p>
+      <p className="container-info-day-p">Heure: </p>
+    </div>
+  );
+}
+
+export default InfoDay;
