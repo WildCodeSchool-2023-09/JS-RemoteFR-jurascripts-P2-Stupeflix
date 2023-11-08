d23088e9d790c34e34d2e938635a08a4bf60b866
Alexandre-27 <alexandre24102000@gmail.com>
2023-11-08 15:57:03 +0100

    Added NavBar component to the wiki page. Stylization of the nav css. Changing the body background in App.scss. Added missing files from latest commit

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,17 +1,24 @@
 import { Link } from "react-router-dom";
+import "../styles/NavBar.scss";
 
 function NavBar() {
   return (
-    <nav>
-      <ul>
-        <li>
-          <Link to="/">Hogwarts Day</Link>
+    <nav className="container-nav">
+      <ul className="container-nav-ul">
+        <li className="container-nav-li">
+          <Link to="/" className="container-nav-link">
+            Hogwarts Day
+          </Link>
         </li>
-        <li>
-          <Link to="/wiki">Wiki</Link>
+        <li className="container-nav-li">
+          <Link to="/wiki" className="container-nav-link">
+            Wiki
+          </Link>
         </li>
-        <li>
-          <Link to="/about">About Us</Link>
+        <li className="container-nav-li">
+          <Link to="/about" className="container-nav-link">
+            About Us
+          </Link>
         </li>
       </ul>
     </nav>
