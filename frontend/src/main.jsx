6e571c4c3a5e9568d5831a84d6713b5ed0dba836
Tetardtek <kvnn64@gmail.com>
2023-11-08 10:14:03 +0100

    Create link for NavBar

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,14 +1,30 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
+import Wiki from "./pages/Wiki";
+import About from "./pages/About";
+import DisplayGame from "./components/game/DisplayGame";
 import App from "./App";
 
 const router = createBrowserRouter([
   {
-    path: "/",
     element: <App />,
+    id: "app",
+    children: [
+      {
+        path: "/",
+        element: <DisplayGame />,
+      },
+      {
+        path: "/wiki",
+        element: <Wiki />,
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+    ],
   },
 ]);
 
