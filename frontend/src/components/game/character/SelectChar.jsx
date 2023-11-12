ee7afae2f5164d403b3514564c7e5399f677a0ee
Alexandre-27 <alexandre24102000@gmail.com>
2023-11-12 10:52:24 +0100

    Changed the css in the SelectChar.scss file, resized cards and images, added color for p tags. Changed the semantics in the SelectChar.jsx file, removed the button tag, added onClick, onKeyDown and the role on the cards div and added 2 p tags.

--- frontend/src/components/game/character/SelectChar.jsx
+++ frontend/src/components/game/character/SelectChar.jsx
@@ -4,54 +4,61 @@ import PropTypes from "prop-types";
 function SelectChar({ setPage }) {
   return (
     <div className="container-game">
+      <h2 className="h2">
+        Clique sur le personnage de ton choix pour l'incarner durant ta partie
+      </h2>
       <div className="container-select">
-        <div className="container-select-1">
+        <div
+          className="container-select-1"
+          onClick={() => setPage({ name: "DisplayQuizz", id: null })}
+          onKeyDown={() => setPage({ name: "DisplayQuizz", id: null })}
+          role="presentation"
+        >
           <img src="harry-potter.jpg" alt="" className="container-select-img" />
-          <button
-            className="container-button"
-            type="button"
-            onClick={() => setPage({ name: "DisplayQuizz", id: null })}
-          >
-            Harry Potter
-          </button>
+          <h2 className="harry-potter-font">Harry Potter</h2>
+          <p>Maison : Gryffondor</p>
+          <p>Caractéristique : 10 points de force supplémentaire</p>
         </div>
-        <div className="container-select-1">
+        <div
+          className="container-select-1"
+          onClick={() => setPage({ name: "DisplayQuizz", id: null })}
+          onKeyDown={() => setPage({ name: "DisplayQuizz", id: null })}
+          role="presentation"
+        >
           <img
             src="luna-lovegood.png"
             alt=""
             className="container-select-img"
           />
-          <button
-            className="container-button"
-            type="button"
-            onClick={() => setPage({ name: "DisplayQuizz", id: null })}
-          >
-            Luna Lovegood
-          </button>
+          <h2 className="harry-potter-font">Luna Lovegood</h2>
+          <p>Maison : Poufsouffle</p>
+          <p>Caractéristique : 10 points de vie supplémentaire</p>
         </div>
-        <div className="container-select-1">
+        <div
+          className="container-select-1"
+          onClick={() => setPage({ name: "DisplayQuizz", id: null })}
+          onKeyDown={() => setPage({ name: "DisplayQuizz", id: null })}
+          role="presentation"
+        >
           <img
             src="cedric-digory.png"
             alt=""
             className="container-select-img"
           />
-          <button
-            className="container-button"
-            type="button"
-            onClick={() => setPage({ name: "DisplayQuizz", id: null })}
-          >
-            Cedric Diggory
-          </button>
+          <h2 className="harry-potter-font">Cedric Diggory</h2>
+          <p>Maison : Serdaigle</p>
+          <p>Caractéristique : 10 points de défense supplémentaire</p>
         </div>
-        <div className="container-select-1">
+        <div
+          className="container-select-1"
+          onClick={() => setPage({ name: "DisplayQuizz", id: null })}
+          onKeyDown={() => setPage({ name: "DisplayQuizz", id: null })}
+          role="presentation"
+        >
           <img src="drago-malfoy.png" alt="" className="container-select-img" />
-          <button
-            className="container-button"
-            type="button"
-            onClick={() => setPage({ name: "DisplayQuizz", id: null })}
-          >
-            Draco Malfoy
-          </button>
+          <h2 className="harry-potter-font">Draco Malfoy</h2>
+          <p>Maison : Serpentard</p>
+          <p>Caractéristique : 10 points inconnu supplémentaire</p>
         </div>
       </div>
     </div>
