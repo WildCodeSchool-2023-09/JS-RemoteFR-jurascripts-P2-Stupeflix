2121b62e7f7fc1f35f0e65d8321a474f7eb69684
Alexandre-27 <alexandre24102000@gmail.com>
2023-11-13 20:34:13 +0100

    Created OpponentFight.jsx component, added OpponentFight.jsx component in DisplayFight.jsx, added div tag in CharFight.jsx component and modified css in DisplayFight.scss and CharFight.scss files to align to the center of the page the CharFight.jsx and OpponentFight.jsx components

--- frontend/src/components/game/fight/DisplayFight.jsx
+++ frontend/src/components/game/fight/DisplayFight.jsx
@@ -1,14 +1,16 @@
 import "../../../styles/DisplayFight.scss";
 import CharFight from "./CharFight";
 import { Fighter, life } from "../character/statChar.cjs";
+import OpponentFight from "./OpponentFight";
 
 function DisplayFight() {
   return (
     <div className="container-game">
-      <h1 className="harry-potter-font fight-h1">
-        Harry Potter VS Ennemi sorcier
-      </h1>
-      <CharFight fighter={Fighter} life={life} />
+      <div className="fgt-div-1">
+        <CharFight fighter={Fighter} life={life} />
+        <h1 className="harry-potter-font fight-vs">VS</h1>
+        <OpponentFight life={life} />
+      </div>
     </div>
   );
 }
