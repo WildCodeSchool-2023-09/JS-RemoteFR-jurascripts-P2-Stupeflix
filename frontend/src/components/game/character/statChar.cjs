c51593980b2ba9f27bc64840e4c1271736910bf4
Alexandre-27 <alexandre24102000@gmail.com>
2023-11-14 01:44:55 +0100

    Creation of the fightTotality function which includes all the combat parameters in the statChar.cjs file. Import of the fightTotality function, implementation of a button which launches the fightTotality function and the isFightEnd function which itself is inside a setTimeout of 10000ms in the DisplayFight.jsx file. The isFightEnd function is used to move on to the next game progress

--- frontend/src/components/game/character/statChar.cjs
+++ frontend/src/components/game/character/statChar.cjs
@@ -2,51 +2,53 @@ let round = 0;
 const lifeData = 10;
 export const life = lifeData.toString();
 
-export class Fighter {
-  constructor(names, strength, dexterity) {
-    this.names = names;
-    this.strength = strength;
-    this.dexterity = dexterity;
-    this.lifeData = lifeData;
+export function fightTotality() {
+  class Fighter {
+    constructor(names, strength, dexterity) {
+      this.names = names;
+      this.strength = strength;
+      this.dexterity = dexterity;
+      this.lifeData = lifeData;
+    }
+
+    fight(defender) {
+      const attackerDamage = Math.round(Math.random() * this.strength);
+      const defenderDamage = Math.max(attackerDamage - defender.dexterity, 1);
+      const def = defender;
+      def.lifeData = Math.max(defender.lifeData - defenderDamage, 0);
+
+      return {
+        attacker: this,
+        defender,
+        attackerDamage,
+        defenderDamage,
+      };
+    }
+
+    isAlive() {
+      return this.lifeData > 0;
+    }
+  }
+  const character = new Fighter("Harry Potter", 15, 3);
+  const enemy = new Fighter("Luna Lovegood", 15, 3);
+
+  function fightRound(attacker, defender) {
+    console.info(`${round}: ${attacker.names} attaque`);
+    console.info(`${attacker.names} ${attacker.lifeData}`);
+    console.info(`${defender.names} ${defender.lifeData}`);
   }
 
-  fight(defender) {
-    const attackerDamage = Math.round(Math.random() * this.strength);
-    const defenderDamage = Math.max(attackerDamage - defender.dexterity, 1);
-    const def = defender;
-    def.lifeData = Math.max(defender.lifeData - defenderDamage, 0);
+  while (character.isAlive() && enemy.isAlive()) {
+    const attacker = round % 2 === 1 ? character : enemy;
+    const defender = round % 2 === 0 ? character : enemy;
+    attacker.fight(defender);
+    round += 1;
 
-    return {
-      attacker: this,
-      defender,
-      attackerDamage,
-      defenderDamage,
-    };
-  }
+    fightRound(round, attacker, defender);
 
-  isAlive() {
-    return this.lifeData > 0;
-  }
-}
-const character = new Fighter("name of char", 15, 3);
-const enemy = new Fighter("name of enemy", 15, 3);
-
-function fightRound(attacker, defender) {
-  console.info(`${round}: ${attacker.names} attaque`);
-  console.info(`${attacker.names} ${attacker.lifeData}`);
-  console.info(`${defender.names} ${defender.lifeData}`);
-}
-
-while (character.isAlive() && enemy.isAlive()) {
-  const attacker = round % 2 === 1 ? character : enemy;
-  const defender = round % 2 === 0 ? character : enemy;
-  attacker.fight(defender);
-  round += 1;
-
-  fightRound(round, attacker, defender);
-
-  if (!defender.isAlive()) {
-    console.info(`${attacker.names} win`);
-    console.info(`${defender.names} lose`);
+    if (!defender.isAlive()) {
+      console.info(`${attacker.names} win`);
+      console.info(`${defender.names} lose`);
+    }
   }
 }
--- frontend/src/components/game/fight/DisplayFight.jsx
+++ frontend/src/components/game/fight/DisplayFight.jsx
@@ -1,16 +1,39 @@
 import "../../../styles/DisplayFight.scss";
+import { useState } from "react";
 import CharFight from "./CharFight";
-import { Fighter, life } from "../character/statChar.cjs";
+import { life, fightTotality } from "../character/statChar.cjs";
 import OpponentFight from "./OpponentFight";
 
 function DisplayFight() {
+  const [fightEnd, setFightEnd] = useState(true);
+  const isFightEnd = () => {
+    setFightEnd(!fightEnd);
+  };
+
   return (
     <div className="container-game">
-      <div className="fgt-div-1">
-        <CharFight fighter={Fighter} life={life} />
-        <h1 className="harry-potter-font fight-vs">VS</h1>
-        <OpponentFight life={life} />
-      </div>
+      {fightEnd ? (
+        <>
+          <div className="fgt-div-1">
+            <CharFight life={life} />
+            <h1 className="harry-potter-font fight-vs">VS</h1>
+            <OpponentFight life={life} />
+          </div>
+          <button
+            type="button"
+            onClick={() => {
+              fightTotality();
+              setTimeout(() => {
+                isFightEnd();
+              }, 10000);
+            }}
+          >
+            Attaquer
+          </button>
+        </>
+      ) : (
+        <h1>Tu as terminé ta partie !</h1>
+      )}
     </div>
   );
 }
