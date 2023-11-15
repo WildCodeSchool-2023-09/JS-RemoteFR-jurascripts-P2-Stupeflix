cd8bd2f91fcb488a58a8cc50cf5ee932388b88ae
Alexandre-27 <alexandre24102000@gmail.com>
2023-11-16 00:44:22 +0100

    Creation of the enemysData.js and charactersData.js files which contains the data of our characters. Implementation of a random function which selects an enemy randomly using data from the enemysData.js file

---
+++ frontend/src/components/game/fight/charactersData.js
@@ -0,0 +1,28 @@
+const characterData = [
+  {
+    name: "Harry Potter",
+    strength: 25,
+    dexterity: 3,
+    life: 20,
+  },
+  {
+    name: "Luna Lovegood",
+    strength: 15,
+    dexterity: 3,
+    life: 30,
+  },
+  {
+    name: "Cedric Diggory",
+    strength: 15,
+    dexterity: 13,
+    life: 20,
+  },
+  {
+    name: "Draco Malfoy",
+    strength: 20,
+    dexterity: 6,
+    life: 26,
+  },
+];
+
+export default characterData;
