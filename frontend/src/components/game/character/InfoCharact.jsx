6d36ed247662af0ef06ea9fec99764818e0d852f
FredP21 <f.pelletier21@gmail.com>
2023-11-15 01:23:15 +0100

    add hook usemediaqueries, fix css on selectchar, wiki, infochar, cardwiki, app, about and quizz

--- frontend/src/components/game/character/InfoCharact.jsx
+++ frontend/src/components/game/character/InfoCharact.jsx
@@ -1,8 +1,10 @@
 import { useState } from "react";
 import "../../../styles/InfoCharact.scss";
 import { BsDash, BsInfo } from "react-icons/bs";
+import { useMediaQuery } from "@react-hook/media-query";
 
 function InfoCharact() {
+  const isMobile = useMediaQuery("only screen and (min-width: 900px)");
   const [infoChar, setInfoChar] = useState(true);
   const toggleInfoChar = () => {
     setInfoChar(!infoChar);
@@ -23,21 +25,45 @@ function InfoCharact() {
           }}
         />
       )}
-      {infoChar && (
-        <div className="div-info">
-          <BsDash
-            className="bsdash"
-            onClick={() => {
-              toggleInfoChar();
-              toggleIconI();
-            }}
-          />
-          <p>Caractéristique de Harry Potter</p>
-          <ul>
-            <li>Points de vie : 10</li>
-            <li>Points de force : 15</li>
-            <li>Points de défense : 3</li>
-          </ul>
+      {isMobile ? (
+        <div>
+          {infoChar && (
+            <div className="div-info">
+              <BsDash
+                className="bsdash"
+                onClick={() => {
+                  toggleInfoChar();
+                  toggleIconI();
+                }}
+              />
+              <p>Statistiques de votre personnage:</p>
+              <ul>
+                <li>Points de vie : 10</li>
+                <li>Points de force : 15</li>
+                <li>Points de défense : 3</li>
+              </ul>
+            </div>
+          )}
+        </div>
+      ) : (
+        <div>
+          {infoChar && (
+            <div className="div-info">
+              <BsDash
+                className="bsdash"
+                onClick={() => {
+                  toggleInfoChar();
+                  toggleIconI();
+                }}
+              />
+              <p>Stats:</p>
+              <ul>
+                <li>Vie: 10</li>
+                <li>Atq: 15</li>
+                <li>Def: 3</li>
+              </ul>
+            </div>
+          )}
         </div>
       )}
     </>
--- frontend/src/components/game/character/SelectChar.jsx
+++ frontend/src/components/game/character/SelectChar.jsx
@@ -1,66 +1,136 @@
 import "../../../styles/SelectChar.scss";
 import PropTypes from "prop-types";
+import { useMediaQuery } from "@react-hook/media-query";
 
 function SelectChar({ setPage }) {
+  const isMobile = useMediaQuery("only screen and (min-width: 900px)");
   return (
     <div className="container-game">
-      <h2 className="h2">
-        Clique sur le personnage de ton choix pour l'incarner durant ta partie
-      </h2>
-      <div className="container-select">
-        <div
-          className="container-select-1"
-          onClick={() => setPage({ name: "DisplayQuizz", id: null })}
-          onKeyDown={() => setPage({ name: "DisplayQuizz", id: null })}
-          role="presentation"
-        >
-          <img src="harry-potter.jpg" alt="" className="container-select-img" />
-          <h2 className="harry-potter-font">Harry Potter</h2>
-          <p>Maison : Gryffondor</p>
-          <p>Caractéristique : 10 points de force supplémentaire</p>
+      {isMobile ? (
+        <>
+          <h2 className="h2">
+            Clique sur le personnage de ton choix pour l'incarner durant ta
+            partie
+          </h2>
+          <div className="container-select">
+            <div
+              className="container-select-1"
+              onClick={() => setPage({ name: "DisplayQuizz", id: null })}
+              onKeyDown={() => setPage({ name: "DisplayQuizz", id: null })}
+              role="presentation"
+            >
+              <img
+                src="harry-potter.jpg"
+                alt=""
+                className="container-select-img"
+              />
+              <h2 className="harry-potter-font">Harry Potter</h2>
+              <p>Maison : Gryffondor</p>
+              <p>Caractéristique : 10 points de force supplémentaire</p>
+            </div>
+            <div
+              className="container-select-1"
+              onClick={() => setPage({ name: "DisplayQuizz", id: null })}
+              onKeyDown={() => setPage({ name: "DisplayQuizz", id: null })}
+              role="presentation"
+            >
+              <img
+                src="luna-lovegood.png"
+                alt=""
+                className="container-select-img"
+              />
+              <h2 className="harry-potter-font">Luna Lovegood</h2>
+              <p>Maison : Poufsouffle</p>
+              <p>Caractéristique : 10 points de vie supplémentaire</p>
+            </div>
+            <div
+              className="container-select-1"
+              onClick={() => setPage({ name: "DisplayQuizz", id: null })}
+              onKeyDown={() => setPage({ name: "DisplayQuizz", id: null })}
+              role="presentation"
+            >
+              <img
+                src="cedric-digory.png"
+                alt=""
+                className="container-select-img"
+              />
+              <h2 className="harry-potter-font">Cedric Diggory</h2>
+              <p>Maison : Serdaigle</p>
+              <p>Caractéristique : 10 points de défense supplémentaire</p>
+            </div>
+            <div
+              className="container-select-1"
+              onClick={() => setPage({ name: "DisplayQuizz", id: null })}
+              onKeyDown={() => setPage({ name: "DisplayQuizz", id: null })}
+              role="presentation"
+            >
+              <img
+                src="drago-malfoy.png"
+                alt=""
+                className="container-select-img"
+              />
+              <h2 className="harry-potter-font">Draco Malfoy</h2>
+              <p>Maison : Serpentard</p>
+              <p>Caractéristique : 10 points inconnu supplémentaire</p>
+            </div>
+          </div>
+        </>
+      ) : (
+        <div className="container-select">
+          <div
+            className="container-select-2"
+            onClick={() => setPage({ name: "DisplayQuizz", id: null })}
+            onKeyDown={() => setPage({ name: "DisplayQuizz", id: null })}
+            role="presentation"
+          >
+            <img
+              src="harry-potter.jpg"
+              alt=""
+              className="container-select-img"
+            />
+            <h2 className="harry-potter-font">Harry Potter</h2>
+          </div>
+          <div
+            className="container-select-2"
+            onClick={() => setPage({ name: "DisplayQuizz", id: null })}
+            onKeyDown={() => setPage({ name: "DisplayQuizz", id: null })}
+            role="presentation"
+          >
+            <img
+              src="luna-lovegood.png"
+              alt=""
+              className="container-select-img"
+            />
+            <h2 className="harry-potter-font">Luna Lovegood</h2>
+          </div>
+          <div
+            className="container-select-2"
+            onClick={() => setPage({ name: "DisplayQuizz", id: null })}
+            onKeyDown={() => setPage({ name: "DisplayQuizz", id: null })}
+            role="presentation"
+          >
+            <img
+              src="cedric-digory.png"
+              alt=""
+              className="container-select-img"
+            />
+            <h2 className="harry-potter-font">Cedric Diggory</h2>
+          </div>
+          <div
+            className="container-select-2"
+            onClick={() => setPage({ name: "DisplayQuizz", id: null })}
+            onKeyDown={() => setPage({ name: "DisplayQuizz", id: null })}
+            role="presentation"
+          >
+            <img
+              src="drago-malfoy.png"
+              alt=""
+              className="container-select-img"
+            />
+            <h2 className="harry-potter-font">Drago Malfoy</h2>
+          </div>
         </div>
-        <div
-          className="container-select-1"
-          onClick={() => setPage({ name: "DisplayQuizz", id: null })}
-          onKeyDown={() => setPage({ name: "DisplayQuizz", id: null })}
-          role="presentation"
-        >
-          <img
-            src="luna-lovegood.png"
-            alt=""
-            className="container-select-img"
-          />
-          <h2 className="harry-potter-font">Luna Lovegood</h2>
-          <p>Maison : Poufsouffle</p>
-          <p>Caractéristique : 10 points de vie supplémentaire</p>
-        </div>
-        <div
-          className="container-select-1"
-          onClick={() => setPage({ name: "DisplayQuizz", id: null })}
-          onKeyDown={() => setPage({ name: "DisplayQuizz", id: null })}
-          role="presentation"
-        >
-          <img
-            src="cedric-digory.png"
-            alt=""
-            className="container-select-img"
-          />
-          <h2 className="harry-potter-font">Cedric Diggory</h2>
-          <p>Maison : Serdaigle</p>
-          <p>Caractéristique : 10 points de défense supplémentaire</p>
-        </div>
-        <div
-          className="container-select-1"
-          onClick={() => setPage({ name: "DisplayQuizz", id: null })}
-          onKeyDown={() => setPage({ name: "DisplayQuizz", id: null })}
-          role="presentation"
-        >
-          <img src="drago-malfoy.png" alt="" className="container-select-img" />
-          <h2 className="harry-potter-font">Draco Malfoy</h2>
-          <p>Maison : Serpentard</p>
-          <p>Caractéristique : 10 points inconnu supplémentaire</p>
-        </div>
-      </div>
+      )}
     </div>
   );
 }
