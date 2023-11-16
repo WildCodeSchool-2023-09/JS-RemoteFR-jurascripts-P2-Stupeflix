685c246be00aa6d9332dccf6ca2c2aa73cc6e3d0
Alexandre-27 <alexandre24102000@gmail.com>
2023-11-16 14:37:53 +0100

    fixed media css in file Player.jsx

--- frontend/src/components/game/fight/Player.jsx
+++ frontend/src/components/game/fight/Player.jsx
@@ -1,7 +1,9 @@
 import "../../../styles/Player.scss";
 import { useState, useEffect } from "react";
+import { useMediaQuery } from "@react-hook/media-query";
 
 function Player() {
+  const isMobile = useMediaQuery("only screen and (min-width: 900px)");
   const [selectedCharacter, setSelectedCharacter] = useState(null);
 
   useEffect(() => {
@@ -14,24 +16,45 @@ function Player() {
   }, []);
 
   return (
-    <div className="opp-fight">
-      {selectedCharacter && (
-        <>
-          <h1 className="harry-potter-font fight-h1">
-            {selectedCharacter.name}
-          </h1>
-          <img src="harry-potter.jpg" alt="" className="fight-img" />
-          <div className="fight-box">
-            <p>{selectedCharacter.name}</p>
-            <div className="fight-rod-life-outer">
-              <div className="fight-rod-life-inner">
-                {selectedCharacter.life}
+    <div>
+      {isMobile ? (
+        <div className="opp-fight">
+          {selectedCharacter && (
+            <>
+              <h1 className="harry-potter-font fight-h1">
+                {selectedCharacter.name}
+              </h1>
+              <img src="harry-potter.jpg" alt="" className="fight-img" />
+              <div className="fight-box">
+                <div className="fight-rod-life-outer">
+                  <div className="fight-rod-life-inner">
+                    {selectedCharacter.life}
+                  </div>
+                </div>
+                <p className="fight-p2">{selectedCharacter.strength}</p>
+                <p className="fight-p-3">{selectedCharacter.dexterity}</p>
               </div>
-            </div>
-            <p className="fight-p2">{selectedCharacter.strength}</p>
-            <p className="fight-p-3">{selectedCharacter.dexterity}</p>
-          </div>
-        </>
+            </>
+          )}
+        </div>
+      ) : (
+        <div className="opp-fight">
+          {selectedCharacter && (
+            <>
+              <img src="harry-potter.jpg" alt="" className="fight-img" />
+              <div className="fight-box">
+                <p>{selectedCharacter.name}</p>
+                <div className="fight-rod-life-outer">
+                  <div className="fight-rod-life-inner">
+                    {selectedCharacter.life}
+                  </div>
+                </div>
+                <p className="fight-p2">{selectedCharacter.strength}</p>
+                <p className="fight-p-3">{selectedCharacter.dexterity}</p>
+              </div>
+            </>
+          )}
+        </div>
       )}
     </div>
   );
